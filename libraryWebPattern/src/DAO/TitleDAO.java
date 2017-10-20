aeda95587ef8bdc0941bf05dd4d45e1496543356
ayeshakhan777 <32134703+ayeshakhan777@users.noreply.github.com>
2017-10-20 11:58:00 +0100

    - updated the TitleDao

--- libraryWebPattern/src/DAO/TitleDAO.java
+++ libraryWebPattern/src/DAO/TitleDAO.java
@@ -1,11 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DAO;
 
 import Dtos.Title;
+import Dtos.User;
 import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 import interfaces.TitleDAOInterface;
 import java.sql.Connection;
@@ -13,43 +9,41 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author Seanoh
+ * @author ayesha
  */
 public class TitleDAO extends DAO implements TitleDAOInterface {
 
+    public TitleDAO(String databaseName) {
+        super(databaseName);
+    }
+
     @Override
-    public ArrayList<Title> getTitlesByDescription(String description) {
+    public ArrayList<Title> viewAllTitles() {
         Connection con = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
-        Title title = null;
         ArrayList<Title> titles = new ArrayList();
 
         try {
             con = getConnection();
-            String query = "SELECT * FROM titles WHERE titleDescription LIKE ?";
-            ps = con.prepareStatement(query);
 
-            ps.setString(1, "%" + description + "%");
+            String query = "SELECT * from titles";
+            ps = con.prepareStatement(query);
             rs = ps.executeQuery();
 
             while (rs.next()) {
-                title = new Title();
-                // Get the pieces of a customer from the resultset
-                title.setNovelName(rs.getString("novelName"));
-                title.setAuthor(rs.getString("author"));
-                title.setStock(rs.getInt("stock"));
-                title.setOnLoan(rs.getInt("onLoan"));
-                title.setTitleDescription(rs.getString("titleDescription"));
-                titles.add(title);
+
+                Title c = new Title(rs.getInt("titleID"), rs.getString("novelName"), rs.getString("author"), rs.getInt("stock"), rs.getInt("onLoan"), rs.getString("titleDecription"));
+                titles.add(c);
             }
 
         } catch (SQLException e) {
-            System.out.println("Exception occured in the getTitlesByDescription() method");
-            e.printStackTrace();
+            System.out.println("Exception occured in the viewAllTitles() method");
         } finally {
             try {
                 if (rs != null) {
@@ -59,270 +53,225 @@ public class TitleDAO extends DAO implements TitleDAOInterface {
                     ps.close();
                 }
                 if (con != null) {
-                    closeConnection(con);
+                    freeConnection(con);
                 }
             } catch (SQLException e) {
-                System.out.println("Exception occured in the finally section in the getTitlesByDescription() method");
+                System.out.println("Exception occured in the finally section in the viewAllTitles() method");
             }
         }
 
         return titles;
     }
 
-    @Override
-    public ArrayList<Title> getTitlesByAuthor(String author) {
+    public boolean addTitle(int titleID, String novelName, int stock, String titleDescription, int definedLoan) {
         Connection con = null;
         PreparedStatement ps = null;
-        ResultSet rs = null;
-        Title title = null;
-        ArrayList<Title> titles = new ArrayList();
-
+        //This if statement will make check if the user is admin which is 1.
         try {
-            con = getConnection();
-            String query = "SELECT * FROM titles WHERE author = ?";
+            con = getConnetion();
+            String query = "INSERT INTO titles (titleID, novelName, stock, titleDescription, definedLoan) VALUES (?, ? , ?, ?, ? )";
             ps = con.prepareStatement(query);
-
-            ps.setString(1, author);
-            rs = ps.executeQuery();
-
-            while (rs.next()) {
-                title = new Title();
-                // Get the pieces of a customer from the resultset
-                title.setNovelName(rs.getString("novelName"));
-                title.setAuthor(rs.getString("author"));
-                title.setStock(rs.getInt("stock"));
-                title.setOnLoan(rs.getInt("onLoan"));
-                title.setTitleDescription(rs.getString("titleDescription"));
-                titles.add(title);
-            }
-
-        } catch (SQLException e) {
-            System.out.println("Exception occured in the getTitlesByAuthor() method");
-            e.printStackTrace();
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-                if (ps != null) {
-                    ps.close();
-                }
-                if (con != null) {
-                    closeConnection(con);
-                }
-            } catch (SQLException e) {
-                System.out.println("Exception occured in the finally section in the getTitlesByAuthor() method");
-            }
-        }
-
-        return titles;
-    }
-
-    @Override
-    public ArrayList<Title> getTitlesByName(String name) {
-        Connection con = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        Title title = null;
-        ArrayList<Title> titles = new ArrayList();
-
-        try {
-            con = getConnection();
-            String query = "SELECT * FROM titles WHERE novelName = ?";
-            ps = con.prepareStatement(query);
-
-            ps.setString(1, "%" + name + "%");
-            rs = ps.executeQuery();
-
-            while (rs.next()) {
-                title = new Title();
-                // Get the pieces of a customer from the resultset
-                title.setNovelName(rs.getString("novelName"));
-                title.setAuthor(rs.getString("author"));
-                title.setStock(rs.getInt("stock"));
-                title.setOnLoan(rs.getInt("onLoan"));
-                title.setTitleDescription(rs.getString("titleDescription"));
-                titles.add(title);
-            }
-
-        } catch (SQLException e) {
-            System.out.println("Exception occured in the getTitlesByName() method");
-            e.printStackTrace();
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-                if (ps != null) {
-                    ps.close();
-                }
-                if (con != null) {
-                    closeConnection(con);
-                }
-            } catch (SQLException e) {
-                System.out.println("Exception occured in the finally section in the getTitlesByName() method");
-            }
-        }
-
-        return titles;
-    }
-
-    @Override
-    public Title getTitleByID(int id) {
-        Connection con = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        Title title = null;
-        try {
-            con = getConnection();
-            String query = "SELECT * FROM titles WHERE novelName = ?";
-            ps = con.prepareStatement(query);
-
-            ps.setInt(1, id);
-            rs = ps.executeQuery();
-
-            while (rs.next()) {
-                title = new Title();
-                title.setNovelName(rs.getString("novelName"));
-                title.setAuthor(rs.getString("author"));
-                title.setStock(rs.getInt("stock"));
-                title.setOnLoan(rs.getInt("onLoan"));
-                title.setTitleDescription(rs.getString("titleDescription"));
-            }
-
-        } catch (SQLException e) {
-            System.out.println("Exception occured in the getTitlesByName() method");
-            e.printStackTrace();
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-                if (ps != null) {
-                    ps.close();
-                }
-                if (con != null) {
-                    closeConnection(con);
-                }
-            } catch (SQLException e) {
-                System.out.println("Exception occured in the finally section in the getTitlesByName() method");
-            }
-        }
-
-        return title;
-    }
-
-    @Override
-    public boolean addTitle(Title title) {
-        Connection con = null;
-        PreparedStatement ps = null;
-        int rs = 0;
-        Title titleTemp = null;
-        Boolean result = null;
-
-        try {
-            con = getConnection();
-            String query = "INSERT INTO users VALUES(NULL,?,?,?,?,?)";
-            ps = con.prepareStatement(query);
-            String novelName = title.getNovelName();
-            String author = title.getAuthor();
-            int stock = title.getStock();
-            int onLoan = title.getOnLoan();
-            String titleDescription = title.getTitleDescription();
-
-            // Fill in the blanks, i.e. parameterize the query
-            ps.setString(1, novelName);
-            ps.setString(2, author);
+            ps.setInt(1, titleID);
+            ps.setString(2, novelName);
             ps.setInt(3, stock);
-            ps.setInt(4, onLoan);
-            ps.setString(5, titleDescription);
-
-            // Execute the query
-            rs = ps.executeUpdate();
-
-        } catch (MySQLIntegrityConstraintViolationException e) {
-            System.out.println("Constraint Exception occurred: " + e.getMessage());
-            // Set the rowsAffected to -1, this can be used as a flag for the display section
-            rs = -1;
-
-        } catch (SQLException se) {
-            System.out.println("SQL Exception occurred: " + se.getMessage());
-            se.printStackTrace();
-        } catch (Exception e) {
-            System.out.println("Exception occurred: " + e.getMessage());
-            e.printStackTrace();
-        } // Now that the program has completed its database access component, 
-        // close the open access points (resultset, preparedstatement, connection)
-        // Remember to close them in the OPPOSITE ORDER to how they were opened
-        // Opening order: Connection -> PreparedStatement -> ResultSet
-        // Closing order: ResultSet -> PreparedStatement -> Connection
-        finally {
+            ps.setString(4, titleDescription);
+            ps.setInt(5, definedLoan);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Exception occured in the addTitle() method: " + e.getMessage());
+            return false;
+        } finally {
             try {
                 if (ps != null) {
                     ps.close();
                 }
                 if (con != null) {
-                    closeConnection(con);
+                    freeConnection(con);
                 }
             } catch (SQLException e) {
-                System.out.println("Exception occured in the finally section in the addTitle() method");
+                System.out.println("Exception occured in the finally section of the addTitle() method: " + e.getMessage());
+                return false;
             }
         }
-
-        if (rs > 0) {
-            result = true;
-        } else {
-            result = false;
-        }
-
-        return result;
+        return true;
     }
 
-    @Override
-    public ArrayList<Title> getAllTitles() {
+    public int deleteTitleById(int titleID) {
         Connection con = null;
         PreparedStatement ps = null;
-        ResultSet rs = null;
-        Title title = null;
-        ArrayList<Title> titles = new ArrayList();
+        int rowsAffected = 0;
+        try {
+            con = getConnection();
+            String query = "DELETE FROM titles WHERE titleID = ?";
+            ps = con.prepareStatement(query);
+            ps.setInt(1, titleID);
+            rowsAffected = ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Exception occured in the deleteTitleById() method: " + e.getMessage());
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (con != null) {
+                    freeConnection(con);
+                }
+            } catch (SQLException e) {
+                System.out.println("Exception occured in the finally section of the deleteTitleById() method");
+                e.getMessage();
+            }
+        }
+        return rowsAffected;
+    }
+
+    public int updateTitleDetails(int titleID, String newGenre, String newNovelName, String author, String newDescripition) {
+
+        Connection con = null;
+        PreparedStatement ps = null;
+        int rowsAffected = 0;
 
         try {
             con = getConnection();
-            String query = "SELECT * FROM titles ";
-            ps = con.prepareStatement(query);
-            rs = ps.executeQuery();
+            String query = "UPDATE titles SET titleID = ?, genre = ?, novelName = ? , author = ? , titleDescription = ?  WHERE titleID = ?";
 
-            while (rs.next()) {
-                title = new Title();
-                title.setTitleID(rs.getInt("titleID"));
-                title.setNovelName(rs.getString("novelName"));
-                title.setAuthor(rs.getString("author"));
-                title.setStock(rs.getInt("stock"));
-                title.setOnLoan(rs.getInt("onLoan"));
-                title.setTitleDescription(rs.getString("titleDescription"));
-                titles.add(title);
-            }
+            ps = con.prepareStatement(query);
+            ps.setInt(1, titleID);
+            ps.setString(2, newGenre);
+            ps.setString(3, newNovelName);
+            ps.setString(4, author);
+            ps.setString(5, newDescripition);
+            rowsAffected = ps.executeUpdate(query);
 
         } catch (SQLException e) {
-            System.out.println("Exception occured in the getTitlesByName() method");
-            e.printStackTrace();
+            System.out.println("Exception occured in the updateTitleDetails() method: " + e.getMessage());
         } finally {
             try {
-                if (rs != null) {
-                    rs.close();
-                }
                 if (ps != null) {
                     ps.close();
                 }
                 if (con != null) {
-                    closeConnection(con);
+                    freeConnection(con);
                 }
             } catch (SQLException e) {
-                System.out.println("Exception occured in the finally section in the getTitlesByName() method");
+                System.out.println("Exception occured in the finally section of the updateTitleDetails() method");
+                e.getMessage();
             }
         }
 
-        return titles;
+        return rowsAffected;
+    }
+
+    public Title searchByAuthor(String author) {
+
+        {
+            Connection con = null;
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+            Title t = null;
+            try {
+                con = getConnection();
+
+                String query = "Select * from titles where author = ?";
+                ps = con.prepareStatement(query);
+                ps.setString(1, author);
+                rs = ps.executeQuery();
+                if (rs.next()) {
+                    t = new Title(rs.getInt("titleID"), rs.getString("novelName"), rs.getString("author"), rs.getInt("stock"), rs.getInt("onLoan"), rs.getString("titleDecription"));
+                }
+            } catch (SQLException e) {
+                System.out.println("Exception occured in the searchByAuthor() method: " + e.getMessage());
+            } finally {
+                try {
+                    if (rs != null) {
+                        rs.close();
+                    }
+                    if (ps != null) {
+                        ps.close();
+                    }
+                    if (con != null) {
+                        freeConnection(con);
+                    }
+                } catch (SQLException e) {
+                    System.out.println("Exception occured in the finally section of the searchByAuthor() method: " + e.getMessage());
+                }
+            }
+            return t;
+        }
+    }
+
+    public Title searchByNovelName(String novelName) {
+        {
+            Connection con = null;
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+            Title t = null;
+            try {
+                con = getConnection();
+
+                String query = "Select * from titles where novelName = ?";
+                ps = con.prepareStatement(query);
+                ps.setString(1, novelName);
+                rs = ps.executeQuery();
+                if (rs.next()) {
+                    t = new Title(rs.getInt("titleID"), rs.getString("novelName"), rs.getString("author"), rs.getInt("stock"), rs.getInt("onLoan"), rs.getString("titleDecription"));
+                }
+            } catch (SQLException e) {
+                System.out.println("Exception occured in the searchByNovelName() method: " + e.getMessage());
+            } finally {
+                try {
+                    if (rs != null) {
+                        rs.close();
+                    }
+                    if (ps != null) {
+                        ps.close();
+                    }
+                    if (con != null) {
+                        freeConnection(con);
+                    }
+                } catch (SQLException e) {
+                    System.out.println("Exception occured in the finally section of the searchByNovelName() method: " + e.getMessage());
+                }
+            }
+            return t;
+        }
+
+    }
+
+    public Title searchByGenre(int genre) {
+        {
+            Connection con = null;
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+            Title t = null;
+            try {
+                con = getConnection();
+
+                String query = "Select * from titlegenre where genreID = ?";
+                ps = con.prepareStatement(query);
+                ps.setInt(1, genre);
+                rs = ps.executeQuery();
+                if (rs.next()) {
+                    t = new Title(rs.getInt("titleID"), rs.getString("novelName"), rs.getString("author"), rs.getInt("stock"), rs.getInt("onLoan"), rs.getString("titleDecription"));
+                }
+            } catch (SQLException e) {
+                System.out.println("Exception occured in the searchByGenre() method: " + e.getMessage());
+            } finally {
+                try {
+                    if (rs != null) {
+                        rs.close();
+                    }
+                    if (ps != null) {
+                        ps.close();
+                    }
+                    if (con != null) {
+                        freeConnection(con);
+                    }
+                } catch (SQLException e) {
+                    System.out.println("Exception occured in the finally section of the searchByGenre() method: " + e.getMessage());
+                }
+            }
+            return t;
+        }
 
     }
 }
